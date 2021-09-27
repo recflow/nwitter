87c06a65d95631f0938e91bb144af14731b8a80d
recflow <chjh9107@gmail.com>
2021-09-27 16:06:07 +0900

    2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter= ({isLoggedIn})=>{
    
     return (
         <Router>
+            {isLoggedIn && <Navigation/>}
             <switch>
                 {isLoggedIn? 
                 ( <>
                     <Route exact path="/">
                         <Home/>
                     </Route>
+                    <Route exact path="/profile">
+                        <Profile/>
+                    </Route>
+                    <Redirect from="*" to="/" />
                 </>) : (
+                    <>
                 <Route exact paht="/"> 
                     <Auth/> 
                 </Route>
+                <Redirect from="*" to="/" />
+                </>
                 )}
             </switch>
         </Router>
